9dfbf442ff3a4f4f667cbac530b86a3cfd40c3c4
Edmilson Pontes <edmilson.pontes@defensoria.mg.def.br>
2025-02-17 19:35:14 -0300

    evolução do monitoramento

--- scripts/set-env-vars.sh
+++ scripts/set-env-vars.sh
@@ -10,6 +10,10 @@ export REDIS_PASSWORD="admin"
 # MongoDB
 export MONGO_URI="mongodb://mongodb:27017/orderdb"
 
+# Datadog
+export DD_API_KEY="ac0cefe18cd0f5b9668044d0211eb824"
+export DD_SITE="us5.datadoghq.com"
+
 # API
 export SERVER_PORT=8080
 
@@ -18,4 +22,5 @@ echo "RABBITMQ_USER=$RABBITMQ_USER"
 echo "RABBITMQ_PASS=$RABBITMQ_PASS"
 echo "REDIS_PASSWORD=$REDIS_PASSWORD"
 echo "MONGO_URI=$MONGO_URI"
+echo "DD_API_KEY=$DD_API_KEY"
 echo "SERVER_PORT=$SERVER_PORT"
